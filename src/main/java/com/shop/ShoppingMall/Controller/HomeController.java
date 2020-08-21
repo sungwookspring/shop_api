01943c16524aa879caae1cc8c3c5e8fe8e551d9d
choisungwook <kgg1959@naver.com>
2020-08-21 15:39:21 +0900

    Add Home Controller
    application.yml설정

--- src/main/java/com/shop/ShoppingMall/Controller/HomeController.java
+++ src/main/java/com/shop/ShoppingMall/Controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.shop.ShoppingMall.Controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Hello {
+@Slf4j
+public class HomeController {
 
-    @GetMapping("index")
-    public String Hello(Model model){
+    @GetMapping("/")
+    public String home(Model model){
+        log.info("[*] home controller");
         return "index.html";
     }
 }
