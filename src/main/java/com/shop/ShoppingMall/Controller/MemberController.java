31ef708432ef966f18942a093b9734f0090f0ccd
choisungwook <kgg1959@naver.com>
2020-08-21 17:41:33 +0900

    Add Register handler

--- src/main/java/com/shop/ShoppingMall/Controller/MemberController.java
+++ src/main/java/com/shop/ShoppingMall/Controller/MemberController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,5 +18,12 @@ public class MemberController {
     public String CreateMemberForm(Model model){
         model.addAttribute("memberForm", new MemberForm());
         return "members/register";
+    }
+
+    @PostMapping("")
+    public String CreateMember(@Valid MemberForm memberform){
+        //to-do 회원가입
+
+        return "redirect:/";
     }
 }
