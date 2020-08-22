941cdaf7f05994c60a9e5e1dc0e8b503c6d70387
choisungwook <kgg1959@naver.com>
2020-08-22 16:00:13 +0900

    Cnage service for Member
    어노테이션 추가

--- src/main/java/com/shop/ShoppingMall/Service/MemberService.java
+++ src/main/java/com/shop/ShoppingMall/Service/MemberService.java
@@ -3,9 +3,13 @@ package com.shop.ShoppingMall.Service;
 import com.shop.ShoppingMall.Repository.MemberRepository;
 import com.shop.ShoppingMall.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class MemberService {
 
     @Autowired
