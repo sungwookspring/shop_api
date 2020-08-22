1555d22bd6aee6e4bf1bb1565c3394264d447182
choisungwook <kgg1959@naver.com>
2020-08-22 18:18:53 +0900

    Update 회원가입<->templates 연동

--- src/main/java/com/shop/ShoppingMall/Controller/MemberController.java
+++ src/main/java/com/shop/ShoppingMall/Controller/MemberController.java
@@ -1,6 +1,10 @@
 package com.shop.ShoppingMall.Controller;
-
+import com.shop.ShoppingMall.Service.MemberService;
+import com.shop.ShoppingMall.domain.Address;
+import com.shop.ShoppingMall.domain.Member;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +13,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 
 @Controller
-@RequiredArgsConstructor
+@Slf4j
 public class MemberController {
-    //to-do memberserivce
-    //    private MemberService;
+//    private final MemberService memberService; <-- 에러발생
+    private MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
 
     @GetMapping("/members/register")
     public String CreateMemberForm(Model model){
@@ -22,7 +32,15 @@ public class MemberController {
 
     @PostMapping("/members/register")
     public String CreateMember(@Valid MemberForm memberform){
-        //to-do 회원가입
+        Address address = new Address(memberform.getCity(), memberform.getStreet(), memberform.getZipcode());
+        Member member = new Member();
+        
+        log.info("회원가입 시작");
+        member.setName(memberform.getName());
+        member.setAddrss(address);
+        // 회원가입
+        memberService.CreateMember(member);
+        log.info("회원가입 성공");
 
         return "redirect:/";
     }
