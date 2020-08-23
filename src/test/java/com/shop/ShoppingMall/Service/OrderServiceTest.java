6f68b6dce048c9cbf7e9a8179470d6a3eaa184b2
choisungwook <kgg1959@naver.com>
2020-08-23 19:26:30 +0900

    Add 상품주문_재고 초과 Test

--- src/test/java/com/shop/ShoppingMall/Service/OrderServiceTest.java
+++ src/test/java/com/shop/ShoppingMall/Service/OrderServiceTest.java
@@ -1,13 +1,12 @@
 package com.shop.ShoppingMall.Service;
 
+import com.shop.ShoppingMall.Execptions.NotEnoughStockException;
 import com.shop.ShoppingMall.Repository.OrderRepository;
 import com.shop.ShoppingMall.domain.Address;
 import com.shop.ShoppingMall.domain.Member;
 import com.shop.ShoppingMall.domain.Order;
 import com.shop.ShoppingMall.domain.OrderStatus;
 import com.shop.ShoppingMall.domain.item.Book;
-import com.shop.ShoppingMall.domain.item.Item;
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,19 +35,10 @@ public class OrderServiceTest {
          * given
          */
         //회원 등록
-        Member member1 = new Member();
-        member1.setName("Test1");
-        member1.setAddress(new Address("서울", "강서구", "123456"));
-
-        em.persist(member1);
+        Member member1 = CreateMember();
 
         //책 등록
-        Book book = new Book();
-        book.setName("ABC");
-        book.setPrice(10000);
-        book.setStockquantity(10);
-
-        em.persist(book);
+        Book book = CreateBook(10000, 10);
 
         /***
          * when
@@ -69,6 +59,20 @@ public class OrderServiceTest {
         assertEquals("남은 책 수량", 8, book.getStockquantity());
     }
 
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과(){
+        //given
+        Member member1 = CreateMember();
+        Book book = CreateBook(10000, 10);
+
+        //when
+        int orderCount = 11;
+        Long orderId = orderService.order(member1.getId(), book.getId(), orderCount);
+
+        //then
+        fail("상품주문_재고수량초과 테스트 실패");
+    }
+
     @Test
     public void 주문취소(){
 
@@ -77,5 +81,24 @@ public class OrderServiceTest {
     @Test
     public void 샘플주문_재고수량초과() throws Exception{
 
+    }
+
+    private Book CreateBook(int price, int stockquantity) {
+        Book book = new Book();
+        book.setName("ABC");
+        book.setPrice(price);
+        book.setStockquantity(stockquantity);
+        em.persist(book);
+
+        return book;
+    }
+
+    private Member CreateMember() {
+        Member member1 = new Member();
+        member1.setName("Test1");
+        member1.setAddress(new Address("서울", "강서구", "123456"));
+        em.persist(member1);
+
+        return member1;
     }
 }
