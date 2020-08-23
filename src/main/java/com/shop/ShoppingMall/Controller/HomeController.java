5d8e125ccab8393e4559ddf0ad19dd9dbf5a301f
choisungwook <kgg1959@naver.com>
2020-08-23 20:30:05 +0900

    Add 관리자 페이지

--- src/main/java/com/shop/ShoppingMall/Controller/HomeController.java
+++ src/main/java/com/shop/ShoppingMall/Controller/HomeController.java
@@ -9,9 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class HomeController {
 
+    /***
+     * 고객 메인 페이지
+     * @param model
+     * @return
+     */
     @GetMapping("/")
     public String home(Model model){
         log.info("[*] home controller");
         return "index.html";
+    }
+
+    /***
+     * 관리자 메인 페이지
+     * @return
+     */
+    @GetMapping("/admin")
+    public String adminHome(){
+        log.info("[*] admin home controller");
+        return "/admin/index.html";
     }
 }
