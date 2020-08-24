7a3ea6e09ae9a22a5ed3e39bcfae45d7a181b9c2
choisungwook <kgg1959@naver.com>
2020-08-25 07:44:09 +0900

    Add 주문검색(기준:이름)

--- src/main/java/com/shop/ShoppingMall/Repository/OrderRepository.java
+++ src/main/java/com/shop/ShoppingMall/Repository/OrderRepository.java
@@ -3,8 +3,10 @@ package com.shop.ShoppingMall.Repository;
 import com.shop.ShoppingMall.domain.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -42,5 +44,50 @@ public class OrderRepository {
                 .setParameter("name", orderSearch.getMemberName())
                 .setMaxResults(1000)
                 .getResultList();
+    }
+
+    /***
+     * 이름으로 모든 주문 검색
+     * @param orderSearch
+     * @return
+     */
+    public List<Order> findAllByString(OrderSearch orderSearch){
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if(orderSearch.getOrderStatus() != null){
+            if(isFirstCondition){
+                jpql += "where";
+                isFirstCondition = false;
+            }else{
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            if(isFirstCondition){
+                jpql += " where";
+                isFirstCondition = false;
+            }else{
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if(orderSearch.getOrderStatus() != null){
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
     }
 }
