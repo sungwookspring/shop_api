3635c9ddbe2b527d4d457a030ee4dcf21eda0679
choisungwook <kgg1959@naver.com>
2020-09-09 13:23:54 +0900

    Add 주문조회api v2: Dto 사용

--- src/main/java/com/shop/ShoppingMall/api/OrderSimpleAPI_Controller.java
+++ src/main/java/com/shop/ShoppingMall/api/OrderSimpleAPI_Controller.java
@@ -2,12 +2,17 @@ package com.shop.ShoppingMall.api;
 
 import com.shop.ShoppingMall.Repository.OrderRepository;
 import com.shop.ShoppingMall.Repository.OrderSearch;
+import com.shop.ShoppingMall.domain.Address;
 import com.shop.ShoppingMall.domain.Order;
+import com.shop.ShoppingMall.domain.OrderStatus;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,4 +24,31 @@ public class OrderSimpleAPI_Controller {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         return all;
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        //1+N 문제 -> 1개를 구하기 위해 N개의 쿼리문 실행(*반복문 횟수)
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                    .map(order -> new SimpleOrderDto(order))
+                    .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+    }
+
 }
