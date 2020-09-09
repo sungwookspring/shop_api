1eb513949ee17b050997f7d6422d03a626d1459b
choisungwook <kgg1959@naver.com>
2020-09-09 12:12:32 +0900

    Fix 주문조회API v1 무한루프

--- src/main/java/com/shop/ShoppingMall/domain/OrderItem.java
+++ src/main/java/com/shop/ShoppingMall/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package com.shop.ShoppingMall.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.shop.ShoppingMall.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
